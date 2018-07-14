3d42f02cb0fb7c9597cf4fc00b36d06365c9c866
gsulc <gordon.sulc@gmail.com>
2018-07-14 18:32:34 -0400

    switched to extension method
    After some thought, it was more elegant to simply add an extension method to the Timer class.

---
+++ src/UnitTests/ExtensionTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Timers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WatchdogDotNet;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ExtensionTests
+    {
+        [TestMethod]
+        public void TestElapsedEventPrevented()
+        {
+            bool timerElapsed = false;
+            var testComplete = new System.Threading.ManualResetEventSlim(false);
+
+            using (Timer watchdog = new Timer(50))
+            {
+                using (Timer resetTimer = new Timer(10))
+                {
+                    using (Timer testLength = new Timer(500))
+                    {
+                        watchdog.Elapsed += (s, e) => { timerElapsed = true; };
+                        resetTimer.Elapsed += (s, e) => { watchdog.Restart(); };
+                        testLength.Elapsed += (s, e) => { testComplete.Set(); };
+
+                        watchdog.Start();
+                        resetTimer.Start();
+                        testLength.Start();
+
+                        testComplete.Wait();
+
+                        watchdog.Stop();
+                        resetTimer.Stop();
+                        testLength.Stop();
+                    }
+                }
+            }
+
+            Assert.IsFalse(timerElapsed);
+        }
+    }
+}
---
+++ src/WatchdogDotNet/WatchdogExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Timers;
+
+namespace WatchdogDotNet
+{
+    public static class WatchdogExtensions
+    {
+        /// <summary>
+        /// Stops and Starts the timer. Used to kick a watchdog to prevent the timer Elapsed event.
+        /// </summary>
+        public static void Restart(this Timer timer)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+    }
+}
